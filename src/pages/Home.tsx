31065e6e6960e001cc27c5fe6575b418a111103e
eduard <eduardbordukov3@gmail.com>
2024-08-13 01:10:32 +0500

    bug fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,10 @@ import style from "./home.module.scss";
 export default function Home() {
   let data: any = useAppSelector((state) => state.repos.repositories); //Получение информации из состояния
 
+  if (data.total_count === 0) {
+    return <div className={style.none}>Ничего не найдено...</div>;
+  }
+
   return (
     <main className={style.main}>
       {/* Отображение приветствия или основного контента в зависимости от того, есть ли в стейте данные */}
